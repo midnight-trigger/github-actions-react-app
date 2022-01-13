1b4cfae523ca339b3a18361febd6afaa104b5971
西脇潤哉 <amities@junya.local>
2022-01-13 12:42:52 +0800

    fix: dedede

--- release.config.js
+++ release.config.js
@@ -8,10 +8,10 @@ module.exports = {
       "@semantic-release/github",
       {
         assets: [
-          {path: "build.zip", label: "Build"},
-          {path: "coverage.zip", label: "Coverage"}
-        ]
-      }
-    ]
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
   ],
 };
